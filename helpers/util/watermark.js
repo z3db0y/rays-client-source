08a3e49171fb46f71168eaa5b31d722898e37fe6
Banksdevy <maximilianerrica@icloud.com>
2023-06-29 02:26:50 +0200

    fix

--- helpers/util/watermark.js
+++ helpers/util/watermark.js
@@ -1,23 +1,24 @@
 const tooltip = document.createElement('div');
-tooltip.className = 'youNewDiv';
+tooltip.className = 'discordBtn';
 tooltip.setAttribute('onmouseenter', 'playTick()');
-tooltip.innerHTML = '<div style="display:inline-block;width:65px;height:65px;background-image:url(client-asset://icon.png);background-size:cover;margin:-5px;margin-bottom:-10px"></div><div class="helpTxtHol" style="margin-left:10px!important">Need help?<div class="helpGuidOpn">Join our discord!</div></div>';
-if(document.getElementById('battlepassAd')) document.getElementById('battlepassAd').insertAdjacentElement('beforebegin', tooltip);
-else document.getElementById('tlInfHold').appendChild(tooltip);
-tooltip.onclick = () => {
-    window.playSelect?.call();
-    window.open('https://discord.gg/C4k9uVnPwP', '_blank');
-};
-
-const watermark = document.createElement('div');
-watermark.innerHTML = '[RAYS] Client';
-watermark.id = 'clientWatermark';
-watermark.style.color = '#fff';
-watermark.style.marginBottom = '5px';
-
-const curGameInfo = document.getElementById('curGameInfo');
-
-new MutationObserver((_, o) => {
-    if(document.getElementById('clientWatermark')) return;
-    curGameInfo.insertAdjacentElement('afterbegin', watermark);
-}).observe(curGameInfo, { childList: true });
+tooltip.innerHTML = '<div style="display:inline-block;width:65px;height:65px;background-image:url(client-asset: icon.png);background-size:cover;margin:-5px;margin-bottom:-10px"></div><div class="helpTxtHol" style="margin-left:10px!important">Need help?<div class="helpGuidOpn">Join our discord!</div></div>';
+ if(document.getElementById('battlepassAd')) document.getElementById('battlepassAd').insertAdjacentElement('beforebegin', tooltip);
+ else document.getElementById('tlInfHold').appendChild(tooltip);
+ tooltip.onclick = () => {
+     window.playSelect?.call();
+     window.open('https://discord.gg/C4k9uVnPwP', '_blank');
+ };
+ 
+ const watermark = document.createElement('div');
+ watermark.innerHTML = '[RAYS] Client';
+ watermark.id = 'clientWatermark';
+ watermark.style.color = '#fff';
+ watermark.style.marginBottom = '5px';
+    
+ 
+ const curGameInfo = document.getElementById('matchInfo');
+ 
+ new MutationObserver((_, o) => {
+     if(document.getElementById('clientWatermark')) return;
+     curGameInfo.insertAdjacentElement('afterbegin', watermark);
+ }).observe(curGameInfo, { childList: true });
